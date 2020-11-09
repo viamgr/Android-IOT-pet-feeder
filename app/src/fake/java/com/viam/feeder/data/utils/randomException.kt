da1bc9b547feec3989b96b1c72dfaf134d99ff6d
viamgr <zvm4zvm@gmail.com>
2020-11-09 17:40:08 +0330

    rename promise to task

--- app/src/fake/java/com/viam/feeder/data/utils/randomException.kt
+++ app/src/fake/java/com/viam/feeder/data/utils/randomException.kt
@@ -6,15 +6,15 @@ import kotlin.random.Random
 
 suspend inline fun <T> randomException(
     delayTime: Int = 5000,
-    possibility: Int = 40,
+    possibility: Int = 20,
     crossinline body: suspend () -> T
 ): T {
     delay(Random.nextInt(0, delayTime).toLong())
     return when (Random.nextInt(0, possibility)) {
-        0 -> {
+        0, 1, 2 -> {
             throw Exception("Random Error")
         }
-        1, 2, 3 -> {
+        3, 4, 5 -> {
             throw ConnectException()
         }
         else -> {
