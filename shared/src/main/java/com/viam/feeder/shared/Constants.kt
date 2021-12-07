9e7a673217df284a66e0042eb010c1e6da6ea1b9
Vahid Moradi <zvm4zvm@gmail.com>
2021-12-07 15:33:02 +0330

    change server ip

--- shared/src/main/java/com/viam/feeder/shared/Constants.kt
+++ shared/src/main/java/com/viam/feeder/shared/Constants.kt
@@ -1,6 +1,6 @@
 package com.viam.feeder.shared
 
-const val API_IP = "193.108.115.160"
+const val API_IP = "2.56.153.79"
 const val API_PORT = 80
 const val API_SOCKET_PORT = 4200
 const val BASE_URL = "http://$API_IP:$API_PORT/"
